e311a54a3e592b34f306c823c6cb6896b7eaa37b
Adrian 'Zniszczo' Zniszczyński <adrian@zniszczynski.pl>
2016-09-28 04:44:19 +0200

    babel

--- tasks/javascript.js
+++ tasks/javascript.js
@@ -1,6 +1,11 @@
 'use strict';
 
 const eslint = require('gulp-eslint');
+const rev = require('gulp-rev');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const rename = require('gulp-rename');
+const babel = require('gulp-babel');
 
 module.exports = gulp => {
 
@@ -8,10 +13,23 @@ module.exports = gulp => {
     return gulp
       .src(['../**/*.js', '!node_modules/**'])
       .pipe(eslint())
-      .pipe(eslint.format())
-      .pipe(eslint.failAfterError());
+      .pipe(babel({
+        presets: ['es2015'],
+      }))
+      .pipe(eslint.format());
   });
 
-  gulp.task('javascript', ['lint']);
-  gulp.task('javascript-prod', []);
+  gulp.task('javascript', ['lint'], () => {
+    gulp.watch('../**/*.js', ['lint']);
+  });
+
+  gulp.task('javascript-prod', ['lint'], () => {
+    return gulp.src('../src/js/**/*.js')
+      .pipe(concat())
+      .pipe(rename('app.min.js'))
+      .pipe(uglify())
+      .pipe(rev())
+      .pipe(gulp.dest('../dist/js'));
+  });
+
 };
