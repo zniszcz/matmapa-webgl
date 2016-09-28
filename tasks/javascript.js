218d307117e5b09559ad0502e040d6aa9b0f1a6f
Adrian 'Zniszczo' Zniszczyński <adrian@zniszczynski.pl>
2016-09-28 17:42:10 +0200

    javascript tasks fixed

--- tasks/javascript.js
+++ tasks/javascript.js
@@ -4,32 +4,41 @@ const eslint = require('gulp-eslint');
 const rev = require('gulp-rev');
 const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
-// const rename = require('gulp-rename');
 const babel = require('gulp-babel');
+const gutil = require('gulp-util');
 
 module.exports = gulp => {
 
+  // -- Unit script tasks
+
   gulp.task('lint', () => {
-    return gulp
-      .src(['../**/*.js', '!node_modules/**'])
+    gulp.watch('./src/**/*.js', ['javascript']);
+
+    return gulp.src('./src/**/*.js')
       .pipe(eslint())
-      .pipe(babel({
-        presets: ['es2015'],
-      }))
-      .pipe(eslint.format());
+      .pipe(eslint.format())
+      .pipe(babel())
+      .pipe(gulp.dest('./dist/js'));
   });
 
-  gulp.task('javascript', ['lint'], () => {
-    gulp.watch('../**/*.js', ['lint']);
-  });
-
-  gulp.task('javascript-prod', ['lint'], () => {
-    return gulp.src('../src/js/**/*.js')
+  gulp.task('lint:prod', () => {
+    return gulp.src('./src/js/**/*.js')
+      .pipe(babel())
       .pipe(concat('app.min.js'))
-      // .pipe(rename('app.min.js'))
       .pipe(uglify())
       .pipe(rev())
-      .pipe(gulp.dest('../dist/js'));
+      .pipe(gulp.dest('./dist/js'));
   });
 
+  // -- Main script tasks
+  
+  gulp.task('javascript', ['lint'], () => {
+    gulp.watch('./src/**/*.js', ['lint']);
+
+    return gutil.log('Task for serving scripts in developer mode.');
+  });
+
+  gulp.task('javascript:prod', ['lint:prod'], () => {
+    return gutil.log('Task for building scripts in production mode.');
+  });
 };
