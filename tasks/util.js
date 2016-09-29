ce1ec2a6f258ee059047d3148b8033d9d3fd898b
Adrian 'Zniszczo' Zniszczyński <adrian@zniszczynski.pl>
2016-09-29 15:30:07 +0200

    Clean task description added

--- tasks/util.js
+++ tasks/util.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const clean = require('gulp-clean');
+const gutil = require('gulp-util');
 
 module.exports = gulp => {
 
   gulp.task('clean:prod', () => {
+    gutil.log('Task for cleaning dist directory.');
     return gulp
       .src('./dist/*', {read: false, force: true})
       .pipe(clean());
