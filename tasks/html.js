b3535a7d417d8eb58c56cf586216ada481804c79
Adrian 'Zniszczo' Zniszczyński <adrian@zniszczynski.pl>
2016-09-29 15:17:22 +0200

    useref changed to include

--- tasks/html.js
+++ tasks/html.js
@@ -1,25 +1,29 @@
 'use strict';
 
-const useref = require('gulp-useref');
+// const useref = require('gulp-useref');
+const inject = require('gulp-inject');
 
 module.exports = gulp => {
 
   // -- Unit html tasks
 
-  gulp.task('copy-html', () => {
+  gulp.task('inject-html', () => {
     return gulp.src('./src/html/**/*.html')
+      .pipe(inject(gulp.src(['./dist/css/*.css', './dist/js/*.js'], {read: false})))
+      .pipe(gulp.dest('./dist'));
+  });
+
+  gulp.task('inject-html:prod', () => {
+    return gulp.src('./src/**/*.html')
+      .pipe(inject(gulp.src(['./dist/css/*.min.css', './dist/js/*.min.js'], {read: false})))
       .pipe(gulp.dest('./dist'));
   });
 
   // -- Main html tasks
 
-  gulp.task('html', ['copy-html'], () => {
-    return gulp.watch('./src/html/**/*.html', ['copy-html']);
+  gulp.task('html', ['inject-html'], () => {
+    return gulp.watch('./src/html/**/*.html', ['inject-html']);
   });
 
-  gulp.task('html:prod', () => {
-    return gulp.src('./src/**/*.html')
-      .pipe(useref())
-      .pipe(gulp.dest('./dist'));
-  });
+  gulp.task('html:prod', ['inject-html:prod']);
 };
