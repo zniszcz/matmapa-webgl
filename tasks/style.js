a88a43e5744237cef5136208bb3cbbccb1c5d936
Adrian 'Zniszczo' Zniszczyński <adrian@zniszczynski.pl>
2016-09-29 15:26:49 +0200

    optimalisation fix from review

--- tasks/style.js
+++ tasks/style.js
@@ -8,15 +8,6 @@ const cssnano = require('cssnano');
 const rename = require('gulp-rename');
 const gutil = require('gulp-util');
 
-const postcssTasks = [
-  autoprefixer(),
-];
-
-const postcssTasksProd = [
-  autoprefixer(),
-  cssnano(),
-];
-
 module.exports = gulp => {
 
   // -- Unit style tasks
@@ -24,14 +15,14 @@ module.exports = gulp => {
   gulp.task('sass', () => {
     return gulp.src('./src/scss/**/*.scss')
       .pipe(sass().on('error', sass.logError))
-      .pipe(postcss(postcssTasks))
+      .pipe(postcss([autoprefixer()]))
       .pipe(gulp.dest('./dist/css'));
   });
 
   gulp.task('sass:prod', () => {
     return gulp.src('./src/scss/**/*.scss')
       .pipe(sass().on('error', sass.logError))
-      .pipe(postcss(postcssTasksProd))
+      .pipe(postcss([autoprefixer(), cssnano()]))
       .pipe(rename('app.min.css'))
       .pipe(rev())
       .pipe(gulp.dest('./dist/css'));
