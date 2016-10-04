109a81a51808f0c1ddc6b59240063a37796f1c72
Adrian 'Zniszczo' Zniszczyński <adrian@zniszczynski.pl>
2016-10-04 16:58:36 +0200

    code cleaned

--- tasks/watch.task.js
+++ tasks/watch.task.js
@@ -4,7 +4,6 @@ const config = require('./config');
 
 module.exports = gulp => {
   gulp.task('watch', () => {
-    
     gulp.watch(config.scss, () => { runSequence('style', 'browser-sync-stream');});
     gulp.watch(config.html, () => { runSequence('html', 'browser-sync-reload');});
     gulp.watch(config.es, () => { runSequence('javascript', 'browser-sync-reload');});
