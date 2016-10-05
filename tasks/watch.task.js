4b205b940e03f82e263ce3f55f67ffc46d045294
Adrian 'Zniszczo' Zniszczyński <adrian@zniszczynski.pl>
2016-10-05 15:47:22 +0200

    gulp watcher fix

--- tasks/watch.task.js
+++ tasks/watch.task.js
@@ -3,9 +3,12 @@ const runSequence = require('run-sequence');
 const config = require('./config');
 
 module.exports = gulp => {
-  gulp.task('watch', () => {
-    gulp.watch(config.glob.scss, done => { runSequence('style', 'browser-sync-reload', done);});
-    gulp.watch(config.glob.html, done => { runSequence('html', 'browser-sync-reload', done);});
-    gulp.watch(config.glob.js, done  => { runSequence('javascript', 'browser-sync-reload', done);});
-  });
+  
+  // -- Unit watcher tasks
+  gulp.task('watch-style', done => { runSequence('style', 'browser-sync-reload', done);});
+  gulp.task('watch-html', done => { runSequence('html', 'browser-sync-reload', done);});
+  gulp.task('watch-javascript', done => { runSequence('javascript', 'browser-sync-reload', done);});
+
+  // -- Main watcher tasks
+  gulp.task('watch', ['watch-style', 'watch-html', 'watch-javascript']);
 }
