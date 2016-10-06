692c5de082de7f2b863c7b8b0435f9687954fd6c
Adrian 'Zniszczo' Zniszczyński <adrian@zniszczynski.pl>
2016-10-06 21:16:20 +0200

    user list working for multiple occurings

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -1,5 +1,26 @@
+/* global document */
+
 'use strict';
 
 (function () {
-  console.log('Dupa, cześć!');
+  // Works for multiple occurings
+  const userList = document.querySelectorAll('.user_list');
+
+  userList.forEach(function (list) {
+    // Assume that order of elements is constant
+    // TODO: dynamicly get proper elements
+
+    const form = list.children.item(0);
+    const ul = list.children.item(1);
+
+    form.addEventListener('submit', event => {
+      event.preventDefault();
+      const input = form.children.item(0);
+      const newElement = document.createElement('li');
+
+      newElement.innerHTML = input.value;
+      ul.appendChild(newElement);
+      input.value = '';
+    });
+  });
 })();
