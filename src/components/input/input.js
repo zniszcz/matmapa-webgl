822a4628fb3005c28d7070838403989d6bf87748
Adrian 'Zniszczo' Zniszczyński <adrian@zniszczynski.pl>
2016-10-11 19:28:16 +0200

    Input class implementation

---
+++ src/components/input/input.js
@@ -0,0 +1,37 @@
+const defaultConfig = {
+  onSubmit: () => {
+    console.log('There\'s no action appended');
+  },
+};
+
+class Input {
+  constructor(config = defaultConfig) {
+    const form = document.createElement('form');
+    const input = document.createElement('input');
+    const button = document.createElement('button');
+
+    form.classList.add('search_box');
+    input.classList.add('search_box__input');
+    button.classList.add('search_box__button');
+
+    button.type = 'submit';
+    button.innerHTML = 'Add';
+
+    form.appendChild(input);
+    form.appendChild(button);
+
+    form.addEventListener('submit', event => {
+      event.preventDefault();
+      const value = input.value.replace(/(<([^>]+)>)/ig, '');
+
+      if (!value) {
+        throw new Error('You are trying to add empty or unsafe record.');
+      }
+
+      config.onSubmit(value);
+      input.value = '';
+    });
+
+    this.node = form;
+  }
+}
