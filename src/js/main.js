43637f1b2c767241e49f2f2184955dd34948c0d6
Adrian 'Zniszczo' Zniszczyński <adrian@zniszczynski.pl>
2016-10-11 19:32:13 +0200

    SPA style calling elements

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,13 @@
 (function () {
   'use strict';
   console.log('Hello world!');
+
+  // Temporary commented - singling sidebar component container out
+  // const sidebar = new Sidebar();
+  const list = new List();
+  const input = new Input({onSubmit: list.add});
+
+  document.body.appendChild(input.node);
+  document.body.appendChild(list.node);
+
 })();
