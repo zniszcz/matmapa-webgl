0ca875b8a751942dd7e48aa7d8e09c1418464d50
Adrian 'Zniszczo' Zniszczyński <adrian@zniszczynski.pl>
2016-10-11 11:58:39 +0200

    clean code - unnecessary else removing, variable names improoved, class names improoved

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -1,12 +1,12 @@
 (function () {
   'use strict';
   // Works for multiple occurings
-  const userList = document.querySelectorAll('.user_list');
+  const userList = document.querySelectorAll('aside.users');
 
-  userList.forEach(function (list) {
-    const form = list.querySelectorAll('form.search_box')[0];
+  userList.forEach(function (container) {
+    const form = container.querySelectorAll('form.search_box')[0];
     const input = form.querySelectorAll('input.search_box__input')[0];
-    const ul = list.querySelectorAll('ul.user_list__list')[0];
+    const list = container.querySelectorAll('ul.users__list')[0];
 
     form.addEventListener('submit', event => {
       event.preventDefault();
@@ -15,15 +15,15 @@
       newElement.innerHTML = input.value.replace(/(<([^>]+)>)/ig, '');
       if (!newElement.innerHTML) {
         throw new Error('You are trying to add empty or unsafe record.');
-      } else {
-        ul.appendChild(newElement);
-        input.value = '';
       }
+
+      list.appendChild(newElement);
+      input.value = '';
     });
 
-    ul.addEventListener('click', event => {
+    list.addEventListener('click', event => {
       if (event.target.tagName.toLowerCase() === 'li') {
-        ul.removeChild(event.target);
+        list.removeChild(event.target);
       }
     });
   });
