08d385c3ae2cc984a5a49a4ec2371dc2b0d10ef9
Adrian 'Zniszczo' Zniszczyński <adrian@zniszczynski.pl>
2016-10-27 22:11:08 +0200

    UserModel API finished

--- src/models/NameModel.js
+++ src/models/NameModel.js
@@ -1,10 +1,35 @@
 (function () {
   'use strict';
 
-  app.userModel = class UserModel extends app.Abstract.Model {
+  app.UserModel = class UserModel extends app.Abstract.Model {
     constructor() {
       super();
+      this.set('name', []);
     }
+    getNode(query) {
+      // I'm trying to avoid allow acces to folowing variable by declaring it each method. Is it a good idea?
+      const store = this.get('name');
+      const result = (query) ? store.filter(elem => (elem === query)) : store;
+
+      return result;
+    }
+    setNode(value) {
+      const store = this.get('name');
+      store.push(value);
+
+      return store;
+    }
+    findNodes(query = '') {
+      const store = this.get('name');
+      const result = store.filter(elem => elem.includes(query));
+
+      return result;
+    }
+    cleanNodes() {
+      this.set('name', []);
+      return this.get('name');
+    }
+
   };
 
 })();
