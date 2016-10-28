6814ed182859b6150db3548f149e0e665affe8b3
Adrian 'Zniszczo' Zniszczyński <adrian@zniszczynski.pl>
2016-10-28 15:43:40 +0200

    models changed

---
+++ src/models/UserCollectionModel.js
@@ -0,0 +1,40 @@
+(function () {
+  'use strict';
+
+  app.UserCollectionModel = class UserCollectionModel extends app.Abstract.Model {
+    constructor() {
+      super();
+      this.set('collection', []);
+      this.addEventListener('delete', item => this.removeItem(item));
+    }
+    getItems() {
+      return this.get('collection');
+    }
+    findItem(query) {
+      const store = this.get('collection');
+      const result = store.filter(elem => (elem.getName() === query));
+
+      return result;
+    }
+    setItem(item) {
+      this.get('collection').push(item);
+      this.fireEvent('change', item);
+      // Allow chaining
+      return this;
+    }
+    removeItem(item) {
+      if (!item) {
+        return;
+      }
+      const index = this.get('collection').indexOf(item);
+      this.get('collection').splice(index, 1);
+      this.fireEvent('change', item);
+    }
+    cleanItems() {
+      this.set('collection', []);
+      return this.get('collection');
+    }
+
+  };
+
+})();
--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -2,41 +2,43 @@
   'use strict';
 
   app.UserModel = class UserModel extends app.Abstract.Model {
-    constructor() {
+    constructor(name = '', surname = '', email = '') {
       super();
-      this.set('name', []);
-    }
-    getItem(query) {
-      const store = this.get('name');
-      const result = (query) ? store.filter(elem => (elem === query)) : store;
+      this.set('name', name);
+      this.set('surname', surname);
+      this.set('email', email);
 
-      return result;
+      return this;
     }
-    setItem(value) {
-      this.get('name').push(value);
-      this.fireEvent('add', value);
-    }
-    removeItem(query) {
-      if (!query) {
-        return;
-      }
-      const elem = this.getItem(query);
-      const index = this.get('name').indexOf(elem);
-      this.remove(index);
-      this.fireEvent('delete', elem);
-    }
-    // Folowing methods are prepparing to develop this structure
-    findItem(query = '') {
-      const store = this.get('name');
-      const result = store.filter(elem => elem.includes(query));
 
-      return result;
-    }
-    cleanItems() {
-      this.set('name', []);
+    getName() {
       return this.get('name');
     }
+    getSurname() {
+      return this.get('surname');
+    }
+    getEmail() {
+      return this.get('email');
+    }
 
+    setName(name) {
+      this.set('name', name);
+      this.fireEvent('change', this.getUser());
+    }
+    setSurname(surname) {
+      this.set('surname', surname);
+      this.fireEvent('change', this.getUser());
+    }
+    setEmail(email) {
+      this.set('email', email);
+      this.fireEvent('change', this.getUser());
+    }
+
+    remove() {
+      const user = this;
+      this.fireEvent('delete', user);
+      app.UserCollectionModel.fireEvent('delete', user);
+    }
   };
 
 })();
